d3e0c8b97c8fb9c86be9eefa18ef38344ea26e86
Alex Chan <czzsunset@gmail.com>
2013-07-09 23:33:02 +0800

    Submit for tomorrow work
    The network sucks!

--- src/com/mollocer/callbackassisstant/CallActivity.java
+++ src/com/mollocer/callbackassisstant/CallActivity.java
@@ -1,7 +1,36 @@
 package com.mollocer.callbackassisstant;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
 
 public class CallActivity extends Activity {
-
+	 
+	private final static String TAG= "CallActivity"; 
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		
+		
+		final Intent intent = getIntent();
+		
+		
+		final String action = intent.getAction();
+		
+		final Uri uri = intent.getData();
+		
+		Log.d(TAG, action);
+		Log.d(TAG, uri.toString() );
+				
+		
+	}
+	
+//	@Override
+//	public void onStart(Intent intent){
+//		
+//	}
 }
--- src/com/mollocer/callbackassisstant/MainActivity.java
+++ src/com/mollocer/callbackassisstant/MainActivity.java
@@ -1,8 +1,14 @@
 package com.mollocer.callbackassisstant;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +16,24 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
+		Button mBtnCall = (Button)findViewById(R.id.btnCall);
+		mBtnCall.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				TextView tv = (TextView)findViewById(R.id.etPhoneNumber);
+				final String phone = (String) tv.getText();
+				Uri  uri = new Uri("tel:"+phone);
+				Intent intent = new Intent(Intent.ACTION_DIAL, );
+				
+				
+				
+			}
+			
+		});
 	}
 
 	@Override
